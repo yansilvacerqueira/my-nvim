e786d5c8e5d9e07b11f4b77fcffce09576660300
Yan Cerqueira <yansilvacerqueira@outlook.com>
2024-10-26 18:33:19 -0300

    feat: adding my neovim config

---
+++ lua/yancerqueira/packer.lua
@@ -0,0 +1,21 @@
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+  
+  use {
+  	'nvim-telescope/telescope.nvim', tag = '0.1.8',
+  	requires = { {'nvim-lua/plenary.nvim'} }
+  }
+  
+  use ({ 
+	"rose-pine/neovim", 
+	as = "rose-pine",
+	config = function()
+		vim.cmd('colorscheme rose-pine')
+	end
+  })
+
+  use ('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+
+end)
